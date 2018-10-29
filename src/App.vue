344cd1383f7e24b90d97a486b0396e8131ad34be
mikenekrasov <m.m.nekrasov@gmail.com>
2018-10-29 17:01:34 +0300

    add index.html package-json src

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<template>
+    <div id="app">
+        <form id="search">
+            <label> Поиск :
+                <input type="text" name="search" v-model="queryFilter">
+                </label>
+        </form>
+        <DataTable
+                :rows = "rows"
+                :columns = "columns"
+                :filter = "queryFilter" />
+    </div>
+</template>
+<script>
+import DataTable from './components/DataTable.vue'
+export default {
+    name: 'App',
+    components: {
+        DataTable
+    },
+    data: function() {
+        return {
+            queryFilter: '',
+            columns: '',
+            rows: [
+            {"id":21421, "reg_number":"А 932 СФ 392", "ignition":0, "speed":0,  "fuel_level":42,   "device":"E0022400", "mileage":3088.89   },
+            {"id":18323, "reg_number":"В 033 ХЕ 323", "ignition":0, "speed":0,  "fuel_level":50,   "device":"E0021645", "mileage":376848    },
+            {"id":15039, "reg_number":"В 053 ОХ 338", "ignition":0, "speed":0,  "fuel_level":null, "device":"E0020433", "mileage":null      },
+            {"id":21426, "reg_number":"В 244 ДА 392", "ignition":0, "speed":0,  "fuel_level":50,   "device":"E0023755", "mileage":31209     },
+            {"id":21424, "reg_number":"Е 348 ЯК 392", "ignition":0, "speed":0,  "fuel_level":16,   "device":"E0023211", "mileage":60596     },
+            {"id":15024, "reg_number":"Е 594 УХ 392", "ignition":1, "speed":7,  "fuel_level":75.6, "device":"E0020818", "mileage":2941.88   },
+            {"id":21420, "reg_number":"Е 828 ТЮ 392", "ignition":0, "speed":0,  "fuel_level":42,   "device":"E0022399", "mileage":51730.89  },
+            {"id":18322, "reg_number":"К 394 ХБ 345", "ignition":0, "speed":0,  "fuel_level":65.2, "device":"E0021643", "mileage":418012.31 },
+            {"id":18311, "reg_number":"К 423 ХЕ 323", "ignition":1, "speed":24, "fuel_level":81.6, "device":"E0021804", "mileage":73040.08  },
+            {"id":15028, "reg_number":"К 804 ЭХ 222", "ignition":0, "speed":0,  "fuel_level":40.4, "device":null,       "mileage":45772.66  },
+            {"id":15031, "reg_number":"К 842 НЯ 222", "ignition":0, "speed":0,  "fuel_level":73.2, "device":null,       "mileage":25968.78  },
+            {"id":15029, "reg_number":"М 282 ВО 222", "ignition":1, "speed":0,  "fuel_level":80.8, "device":"E0020820", "mileage":84675.14  },
+            {"id":18321, "reg_number":"М 292 ТЫ 222", "ignition":0, "speed":0,  "fuel_level":100,  "device":"E0021642", "mileage":281537    },
+            {"id":15027, "reg_number":"М 839 РЫ 222", "ignition":0, "speed":0,  "fuel_level":81.2, "device":"E0020766", "mileage":84479.19  },
+            {"id":18310, "reg_number":"Н 523 ХА 323", "ignition":1, "speed":3,  "fuel_level":94.8, "device":"E0021636", "mileage":296834.64 },
+            {"id":18316, "reg_number":"О 082 ПЕ 354", "ignition":0, "speed":0,  "fuel_level":140,  "device":"E0021975", "mileage":207013    },
+            {"id":18308, "reg_number":"О 232 НЯ 222", "ignition":1, "speed":7,  "fuel_level":81.6, "device":"E0020823", "mileage":158260.54 },
+            {"id":15042, "reg_number":"О 302 РР 222", "ignition":0, "speed":0,  "fuel_level":null, "device":"E0020436", "mileage":null      },
+            {"id":18312, "reg_number":"О 343 ОУ 222", "ignition":0, "speed":0,  "fuel_level":94,   "device":"E0021812", "mileage":262736.05 },
+            {"id":18324, "reg_number":"О 480 ЕС 222", "ignition":1, "speed":55, "fuel_level":100,  "device":"E0021646", "mileage":375246.53 },
+            {"id":18309, "reg_number":"Р 443 ЧБ 250", "ignition":1, "speed":10, "fuel_level":67.2, "device":"E0021196", "mileage":330.43    },
+            {"id":15072, "reg_number":"С 234 РА 222", "ignition":1, "speed":0,  "fuel_level":null, "device":null,       "mileage":null      },
+            {"id":21423, "reg_number":"С 330 ВД 354", "ignition":0, "speed":0,  "fuel_level":44,   "device":"E0022924", "mileage":24540     },
+            {"id":21419, "reg_number":"Т 430 СР 222", "ignition":0, "speed":0,  "fuel_level":11,   "device":"E0022398", "mileage":33771.89  },
+            {"id":18317, "reg_number":"Т 524 ЧБ 323", "ignition":0, "speed":0,  "fuel_level":172,  "device":"E0021976", "mileage":488132.09 },
+            {"id":21418, "reg_number":"Т 580 УК 222", "ignition":0, "speed":0,  "fuel_level":27,   "device":"E0022396", "mileage":21992.5   },
+            {"id":15030, "reg_number":"У 302 ГО 328", "ignition":0, "speed":0,  "fuel_level":53.2, "device":null,       "mileage":29597.09  },
+            {"id":15026, "reg_number":"У 333 АХ 328", "ignition":0, "speed":0,  "fuel_level":100,  "device":null,       "mileage":63593.1   },
+            {"id":21425, "reg_number":"У 393 БА 328", "ignition":0, "speed":0,  "fuel_level":22,   "device":"E0023539", "mileage":38710     },
+            {"id":18319, "reg_number":"У 442 ЧЕ 323", "ignition":1, "speed":5,  "fuel_level":44.8, "device":"E0022015", "mileage":169384    },
+            {"id":21427, "reg_number":"У 534 КМ 323", "ignition":0, "speed":0,  "fuel_level":30,   "device":"E0023771", "mileage":38403     },
+            {"id":15078, "reg_number":"У 553 НО 323", "ignition":0, "speed":0,  "fuel_level":null, "device":"E0009561", "mileage":null      },
+            {"id":18313, "reg_number":"Х 204 НУ 323", "ignition":0, "speed":127,"fuel_level":65,   "device":"E0021966", "mileage":482543.31 },
+            {"id":21422 ,"reg_number":"Х 382 КУ 323" ,"ignition":0 ,"speed":0 , "fuel_level":29 ,  "device":"E0022403", "mileage":28030     }
+            ]
+    }
+    }
+}
+</script>
---
+++ src/components/DataTable.vue
@@ -0,0 +1,97 @@
+<template>
+    <table>
+        <thead>
+            <tr>
+            <th v-for="col in columns">{{col}}</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="row in filtered" v-bind:class = "{ 'selected': clicked }" v-on:click="toggle(clicked)">
+            <td v-for="col in columns">{{row[col]}}</td>
+            </tr>
+        </tbody>     
+    </table>
+</template>
+<script>
+export default {
+    name: "data-table",
+    props:{
+        rows: Array,
+        filter: String,
+    },
+    data: function(){
+        return {
+            clicked: false
+        }
+    },
+    computed: {
+        columns: function () {
+          if (this.rows.length == 0) {
+            return [];
+          }
+            return Object.keys(this.rows[0]).filter(function(key) {
+                //return key !== 'id'
+                return ["id"].indexOf(key) === -1;
+            }
+        );
+        },
+        filtered: function (filter) {
+            var filter = this.filter && this.filter.toLowerCase();
+          var data = this.rows;
+          if (filter) {
+              data = data.filter(function (row) {
+                return Object.keys(row).some(function (key) {
+                  return String(row[key]).toLowerCase().indexOf(filter) > -1
+                })
+              })
+              return data;
+        }else{
+            return data;
+        }
+        }
+    },
+    methods: {
+        toggle: function (clicked) {
+                clicked = !clicked
+                console.log(clicked)
+            }
+    }
+}
+</script>
+<style>
+    table {
+  font-family: 'Open Sans', sans-serif;
+  width: 750px;
+  border-collapse: collapse;
+  border: 3px solid #44475C;
+  margin: 10px 10px 0 10px;
+}
+
+table th {
+  text-transform: uppercase;
+  text-align: left;
+  background: #44475C;
+  color: #FFF;
+  padding: 8px;
+  min-width: 30px;
+}
+
+table td {
+  text-align: left;
+  padding: 8px;
+  border-right: 2px solid #7D82A8;
+  border-bottom: 2px solid #7D82AB;
+}
+table td:last-child {
+  border-right: none;
+}
+/* table tbody tr:nth-child(2n) td {
+  background: #FFFFFF;
+} */
+table tr{
+    cursor: pointer;
+}
+.selected {
+    background: red;
+}
+</style>
